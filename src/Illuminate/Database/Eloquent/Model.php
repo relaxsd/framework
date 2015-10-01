168a3948daf5a99fa923788e03a9a1f48beceaf6
Martijn Kriek <martijn.kriek@relax-software.nl>
2015-10-01 21:18:47 +0200

    Make SoftDeletes::forceDelete() return the bool Model::delete() result

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -1175,7 +1175,7 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
      *
      * This method protects developers from running forceDelete when trait is missing.
      *
-     * @return void
+     * @return bool|null
      */
     public function forceDelete()
     {
